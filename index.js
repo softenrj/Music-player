8cc5fbba6a1ad405b4b9772aa2eabd15f6614c8d
Raj Sharma <149652817+softenrj@users.noreply.github.com>
2025-01-14 23:36:00 +0530

    Update index.js
    swipe gesture

--- index.js
+++ index.js
@@ -1,303 +1,92 @@
-var musicIndex = Math.floor(Math.random() * list.length);
-var aud = new Audio(`./assets/music/${list[musicIndex].music}`);
+<!DOCTYPE html>
+<html lang="en">
 
-// Function to set the audio source
-function musicSRC(i) {
-    if (!aud.paused) aud.pause(); // Stop the current audio if playing
-    aud.src = `./assets/music/${list[i].music}`; // Update the source of the existing Audio object
-    aud.load(); // Load the new source
-    if (playp.classList.contains("paus")) {
-        aud.play();
-    }
-    aud.addEventListener("ended", endHandle);
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Music Player</title>
+    <link rel="icon" href="./assets/channels4_profile.jpg" type="image/gif" sizes="16x16">
+</head>
 
-// Function to update the images
-function image(i) {
-    var images = document.querySelectorAll(".img");
-    images[0].setAttribute("src", `./assets/music-img/${list[i].img}`);
-    images[1].setAttribute("src", `./assets/music-img/${list[i].img}`);
-}
+<body>
 
-// Load initial image
-image(musicIndex);
-var playp = document.querySelector(".fa-play")
-// Play/Pause button click event
-playp.addEventListener("click", () => {
-    if (playp.classList.contains("paus")) {
-        pause();
-    } else {
-        pla();
-    }
-});
-
-// Play function
-function pla() {
-    playp.classList.add("paus");
-    playp.classList.replace("fa-play", "fa-pause");
-    if (aud.ended || aud.currentTime === 0) {
-        musicSRC(musicIndex);
-        pro();
-    }
-    aud.play();
-    waveFix();
-}
-function waveFix() {
-    document.querySelector("#wave").classList.toggle("loader");
-    document.querySelector(".container").classList.toggle("containerwave");
-    document.querySelector(".player-img").classList.toggle("player-imgwave");
-    document.querySelector(".player-controls").classList.toggle("player-controlswave");
-    document.querySelector(".player-progress").classList.toggle("player-progresswave");
+    <div class="background">
+        <img src="" id="bg-img" class="img">
+    </div>
 
 
-}
+        <div class="container">
+            <div class="player-img">
+                <img src="" class="active img" id="cover">
+            </div>
+            <div id="wave">
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+                <span class="stroke"></span>
+            </div>
+    
+            <div class="player-progress" id="player-progress">
+                <div class="progress" id="progress"></div>
+                <div class="music-duration">
+                    <span id="current-time">0:00</span>
+                    <span id="duration">0:00</span>
+                </div>
+            </div>
+            <div class="volume">
+                <p id="volume_show">100</p>
+                <i class="fa fa-volume-up" aria-hidden="true"  id="volume_icon"></i>
+                <input type="range" min="0" max="100" value="100"  id="volume-changer">  
+            </div>
+    
+            <div class="player-controls">
+                <i class="fa-solid fa-repeat"></i>
+                <i class="fa-solid fa-backward" title="Previous" id="prev"></i>
+                <i class="fa-solid fa-play play-button" title="Play" id="play"></i>
+                <i class="fa-solid fa-forward" title="Next" id="next"></i>
+                <i class="fa-solid fa-bars"></i>
+            </div>
 
-// Pause function
-function pause() {
-    playp.classList.remove("paus");
-    playp.classList.replace("fa-pause", "fa-play");
-    aud.pause();
-    waveFix();
-}
-
-// Progress bar update function
-var progress = document.querySelector(".progress");
-function pro() {
-    setInterval(() => {
-        if (aud.duration) {
-            var musicTimer = (aud.currentTime / aud.duration) * 100;
-            progress.style.width = `${musicTimer}%`;
-        }
-    }, 1000);
-}
-
-// Progress bar click event to seek audio
-var proBar = document.querySelector(".player-progress");
-proBar.addEventListener("click", (event) => {
-    var proBarWidth = proBar.offsetWidth;
-    var clickPosition = (event.offsetX / proBarWidth);
-    aud.currentTime = (clickPosition) * aud.duration;
-    progress.style.width = `${clickPosition * 100}%`;
-});
-
-// Forward button click event
-document.querySelector(".fa-forward").addEventListener("click", () => {
-    musicIndex = (musicIndex + 1);
-    if (musicIndex >= list.length) musicIndex = 0; // Loop back to the first song if at the end
-    image(musicIndex);
-    musicSRC(musicIndex);
-    if (playp.classList.contains("paus")) {
-        aud.play();
-        pro();
-    }
-});
-
-var isDragging = false;
-
-function setAudioProgress(position) {
-    var proBarWidth = proBar.offsetWidth;
-    var clickPosition = (position / proBarWidth);
-    var newTime = (clickPosition) * aud.duration;
-    aud.currentTime = newTime;
-    progress.style.width = `${clickPosition * 100}%`;
-}
-
-function handleDown(event) {
-    isDragging = true;
-    document.addEventListener("mousemove", handleMove);
-    document.addEventListener("mouseup", handleUp);
-
-    document.addEventListener("touchmove", handleMove);
-    document.addEventListener("touchend", handleUp);
-}
-
-function handleMove(event) {
-    if (isDragging) {
-        var proBarRect = proBar.getBoundingClientRect();
-        var position = event.type.includes('mouse') ? event.clientX - proBarRect.left : event.touches[0].clientX - proBarRect.left;
-        if (position >= 0 && position <= proBarRect.width) {
-            setAudioProgress(position);
-        }
-    }
-}
-
-function handleUp() {
-    isDragging = false;
-
-    // Remove event listeners when dragging ends
-    document.removeEventListener("mousemove", handleMove);
-    document.removeEventListener("mouseup", handleUp);
-    document.removeEventListener("touchmove", handleMove);
-    document.removeEventListener("touchend", handleUp);
-}
-
-// Attach event listeners
-proBar.addEventListener("mousedown", handleDown);
-proBar.addEventListener("touchstart", handleDown);
-
-
-
-// Backward button click event
-document.querySelector(".fa-backward").addEventListener("click", () => {
-    if (Math.floor(aud.currentTime) === 0) {
-        musicIndex -= 1;
-        if (musicIndex < 0) musicIndex = list.length - 1;
-        image(musicIndex);
-        musicSRC(musicIndex);
-        if (playp.classList.contains("paus")) {
-            aud.play();
-            pro();
-        }
-    }
-    aud.currentTime = 0;
-});
-
-// Repeat button click event
-var repeat = document.querySelector(".fa-repeat");
-var isrepeat = false;
-repeat.addEventListener("click", () => {
-    isrepeat = !isrepeat;
-    repeat.classList.toggle("loop", isrepeat);
-});
-
-// Handle the end of the audio
-function endHandle() {
-    if (isrepeat) {
-        aud.currentTime = 0;
-        aud.play();
-    } else {
-        musicIndex += 1;
-        if (musicIndex >= list.length) musicIndex = 0; // Loop back to the first song if at the end
-        image(musicIndex);
-        musicSRC(musicIndex);
-        if (playp.classList.contains("paus")) {
-            aud.play();
-            pro();
-        }
-    }
-}
-
-setInterval(() => {
-    const currentTimeMinutes = Math.floor(aud.currentTime / 60);
-    const currentTimeSeconds = Math.floor(aud.currentTime % 60);
-    const durationMinutes = Math.floor(aud.duration / 60);
-    const durationSeconds = Math.floor(aud.duration % 60);
-
-    document.querySelector("#current-time").innerHTML = `${currentTimeMinutes}:${currentTimeSeconds.toString().padStart(2, '0')}`;
-    document.querySelector("#duration").innerHTML = `${durationMinutes}:${durationSeconds.toString().padStart(2, '0')}`;
-}, 100);
-
-document.querySelector(".fa-volume-up").addEventListener("click", toggleMute);
-
-function toggleMute() {
-    if (this.classList.contains("fa-volume-xmark")) {
-        this.classList.replace("fa-volume-xmark", "fa-volume-up");
-        aud.volume = 1;
-        soundBar.value = 100;
-        document.querySelector("#volume_show").innerHTML = 100;
-
-    } else {
-        this.classList.replace("fa-volume-up", "fa-volume-xmark");
-        aud.volume = 0;
-        soundBar.value = 0;
-        document.querySelector("#volume_show").innerHTML = 0;
-
-    }
-}
-var soundBar = document.querySelector("#volume-changer");
-
-soundBar.onchange = function () {
-    const volumeValue = this.value;
-    console.log(volumeValue);
-    document.querySelector("#volume_show").innerHTML = volumeValue;
-    aud.volume = volumeValue / 100;
-};
-
-var musicList = document.querySelector(".music-list");
-document.querySelector(".fa-bars").addEventListener("click", () => {
-    musicList.classList.add("show");
-    musicList.style.height = "300px";
-    musicList.style.opacity = 1;
-
-})
-document.querySelector(".fa-x").addEventListener("click", () => {
-    musicList.classList.remove("show");
-})
-
-function populateMusic() {
-    const musicList = document.querySelector(".music-list ol");
-    musicList.innerHTML = ""; // Clear the list
-
-    for (var i = 0; i < list.length; i++) {
-        (function (index) {
-            const ms = new Audio(`./assets/music/${list[index].music}`);
-            ms.addEventListener('loadedmetadata', function () {
-                var min = Math.floor(ms.duration / 60);
-                var sec = Math.floor(ms.duration % 60);
-
-                const listItem = document.createElement("li");
-                listItem.innerHTML = ` 
-                    <div class="row">
-                        <span>${list[index].name}</span>
-                        <p>${list[index].auth}</p>
+            
+            <div class="music-list">
+                <hr class="close-tab">
+                <div class="header">
+                    <div class="rowed">
+                        <i class="fa-solid fa-music"></i>
+                        <span>Music list</span>
+                        <i class="fa-solid fa-x"></i>
                     </div>
-                    <span class="audio-duration">${min}:${sec.toString().padStart(2, '0')}</span>
-                `;
+                    
+                </div>
+                <ol>
+                    <!-- here li list are coming from js -->
+    
+                    <li>
+                        <div class="row">
+                            <span>Despacito</span>
+                            <p>Justin</p>
+                        </div>
+                        <span class="audio-duration">2:34</span>
+                    </li>
+                    <hr>                    
+                    
+                    
+                </ol>
+            </div>
 
-                listItem.addEventListener("click", () => {
+        </div>
 
-                    musicSRC(index);
-                    image(index);
-                    musicIndex = index;
 
-                })
-                musicList.appendChild(listItem);
-                const hr = document.createElement('hr');
-                hr.className = "music-partition";
-                musicList.appendChild(hr); // Add a horizontal rule
-            });
-            ms.load();
-        })(i);
-    }
+    <script src="./musiclist.js"></script>
+    <script src="index.js"></script>
+</body>
 
-}
-
-//swipe gesture
-const swipe = document.querySelector('.close-tab');
-
-if (swipe) {
-    let startY = 0;
-    const minHeight = 120; // Minimum height for the music list
-    const maxHeight = 300; // Maximum allowed height for the music list
-    const baseHeight = 300; // Default height when fully visible
-
-    swipe.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        startY = e.touches[0].clientY;
-    });
-
-    swipe.addEventListener('touchmove', (e) => {
-        const moveY = e.touches[0].clientY;
-        let distance = startY - moveY + baseHeight;
-
-        // Constrain height between min and max
-        distance = Math.max(minHeight, Math.min(maxHeight, distance));
-
-        musicList.style.height = distance + 'px';
-
-        // Calculate opacity (1 when fully visible, fades to 0 when near minimum height)
-        let opacityValue = (distance - minHeight) / (baseHeight - minHeight);
-        musicList.style.opacity = Math.max(0, Math.min(1, opacityValue));
-    });
-
-    swipe.addEventListener('touchend', () => {
-        const computed_height = window.getComputedStyle(musicList).height;
-        const computed_height_value = parseFloat(computed_height);
-        if (computed_height_value <= 130) {
-            musicList.classList.toggle('show');
-        }
-    });
-} else {
-    console.error("Element not found!");
-}
-window.onload = populateMusic();
+</html>
